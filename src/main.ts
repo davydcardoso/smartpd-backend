b9f20c09ba896c93952e430673890adc5d280504
Davyd Cardoso <davydkewen@gmail.com>
2023-07-20 11:20:50 -0300

    feature: create route for get version data

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { join } from 'path';
 
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
+import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -14,6 +15,19 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', '..', 'upload'), {
     prefix: '/v1/smartpd/public',
   });
+
+  app.use(
+    '/v1/smartpd/test',
+    (req: Request, res: Response, next: NextFunction) => {
+      return res.status(200).send({
+        developer: 'Davyd Cardoso',
+        email: 'contato@davydcardoso.com',
+        version: '0.0.1',
+        lastUpdated: '2023-7-20 11:19',
+        application: "SmartPD - Prodata"
+      });
+    },
+  );
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
